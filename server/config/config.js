ee51bb5c70d9460fbfb405f8b61f06780e707b80
Pedro Avaria <pavaria@mesos.cl>
2020-04-10 00:44:58 -0400

    ResServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,15 @@
 // Puerto
 // ==================
 process.env.PORT = process.env.PORT || 3000
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://strider:KfMxHtswut1yQXqw@cluster0-1rn9b.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB
+
